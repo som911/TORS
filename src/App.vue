5245eed25ca1da4e62d1a9ac46ab9c054da159bf
som911 <mycountwork@gmail.com>
2025-12-16 18:03:04 +0200

    Start

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ hello }}</p>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+const hello: string = "Hello, Vue 3 with TypeScript!";
+</script>
+
+<style lang="scss">
+    p {
+        color: var(--w1);
+    }
+</style>
